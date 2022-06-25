775f13be398f690abb108dab81ed453fc0e679a0
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-06-25 15:47:51 -0500

    inital commit

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,18 @@
 // Write your tests here
+const request = require('supertest')
+const server = require('./server')
+const db = require('../data/dbConfig')
+
+const userA = { username: 'foo', password: 'bar' }
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+afterAll(async () => {
+  await db.destroy()
+})
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).not.toBe(false)
 })
