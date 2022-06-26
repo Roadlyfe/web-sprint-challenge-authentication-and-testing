6ef5fc14fcc1cd4efe8cc3094e7000f81fea81e2
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-06-26 15:01:39 -0500

    index.js fleshed

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const PORT = process.env.PORT || 9000;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
+
+module.exports = {
+  BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS || 8,
+  JWT_SETCRET: 'shh',
+}
