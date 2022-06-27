20e7fe2033766635952f7759989dbfebec5dbb1c
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-06-27 16:16:46 -0500

    wip

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,4 +1,17 @@
+const jwt = require("jsonwebtoken");
+
 const restricted = (req, res, next) => {
+  console.log(req.headers.authorization)
+  if(!req.headers.authorization) {
+    res.status(400).json({message: "token required"})
+  }
+  const token = req.headers.authorization.split(' ')[1]
+  const verified = jwt.verify(token, process.env.JWT_SECRET, (err, decoded) =>{
+    if(err) {
+      res.status(400).json({message: "token invalid"})
+    }
+
+  } )
   next()
   /*
     IMPLEMENT
